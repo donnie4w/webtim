07338e0ebc56a6056fc40cf4c06f489c08e61375
donnie <donnie4w@gmail.com>
2023-12-06 23:24:34 +0800

    Create t.js

---
+++ html/assets/js/t.js
@@ -0,0 +1,7 @@
+var _hmt = _hmt || [];
+(function() {
+var hm = document.createElement("script");
+hm.src = "https://hm.baidu.com/hm.js?cd79b2e3e2e2d7350a9af7cfedbcd16d";
+var s = document.getElementsByTagName("script")[0];
+s.parentNode.insertBefore(hm, s);
+})();
